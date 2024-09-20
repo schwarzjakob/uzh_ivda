dee3f4b6b6d0eacebfd541d5510c2f294c976ecd
Jakob Schwarz <jakob.schwarz97@gmail.com>
2024-09-20 09:27:24 +0200

    Setup database

--- example-project/hello-world/services/backend/src/__init__.py
+++ example-project/hello-world/services/backend/src/__init__.py
@@ -1,14 +1,36 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_restx import Resource, Api
+from flask_pymongo import PyMongo
+from pymongo.collection import Collection
+from .model import Company
 
-# instantiate the app
+# Configure Flask & Flask-PyMongo:
 app = Flask(__name__)
+# allow access from any frontend
+cors = CORS()
+cors.init_app(app, resources={r"*": {"origins": "*"}})
+# add your mongodb URI
+app.config["MONGO_URI"] = "mongodb://localhost:27017/companiesdatabase"
+pymongo = PyMongo(app)
+# Get a reference to the companies collection.
+companies: Collection = pymongo.db.companies
 api = Api(app)
 
 
-class Ping(Resource):
+class CompaniesList(Resource):
     def get(self):
-        return {"status": "success", "message": "pong!"}
+        cursor = companies.find()
+        return [Company(**doc).to_json() for doc in cursor]
 
 
-api.add_resource(Ping, "/ping")
+class Companies(Resource):
+    def get(self, id):
+        cursor = companies.find_one_or_404({"id": id})
+        company = Company(**cursor)
+        # do preprocessing, machine learning etc.
+        return company.to_json()
+
+
+api.add_resource(CompaniesList, "/companies")
+api.add_resource(Companies, "/companies/<int:id>")
