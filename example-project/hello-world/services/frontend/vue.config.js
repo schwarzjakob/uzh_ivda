f5da07271bd498465380f1a0d354351277e07a84
Jakob Schwarz <jakob.schwarz97@gmail.com>
2024-09-20 17:19:54 +0200

    Fix config error

--- example-project/hello-world/services/frontend/vue.config.js
+++ example-project/hello-world/services/frontend/vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
+const webpack = require("webpack");
+
 module.exports = defineConfig({
   transpileDependencies: true,
 
@@ -7,5 +9,14 @@ module.exports = defineConfig({
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
     },
   },
-});
 
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        '__VUE_OPTIONS_API__': JSON.stringify(true),
+        '__VUE_PROD_DEVTOOLS__': JSON.stringify(false),
+        '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': JSON.stringify(false),
+      }),
+    ],
+  },
+});
