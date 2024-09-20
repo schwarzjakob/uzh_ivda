0c7a9b82baddd19ea07bfaf1ed8ee11ab9bfdf2e
Jakob Schwarz <jakob.schwarz97@gmail.com>
2024-09-20 17:52:34 +0200

    Add colors to the scatter plot

--- example-project/hello-world/services/frontend/src/components/ScatterPlot.vue
+++ example-project/hello-world/services/frontend/src/components/ScatterPlot.vue
@@ -8,8 +8,10 @@
     </div>
   </div>
 </template>
+
 <script>
 import Plotly from "plotly.js/dist/plotly";
+
 export default {
   name: "ScatterPlot",
   props: ["selectedCategory"],
@@ -17,25 +19,30 @@ export default {
     selectedCategory: function () {
       this.ScatterPlotData.x = [];
       this.ScatterPlotData.y = [];
-
+      this.ScatterPlotData.name = []; // Ensure to clear the names as well
+      this.ScatterPlotData.color = [];
       this.fetchData();
     },
   },
   data: () => ({
-    ScatterPlotData: { x: [], y: [], name: [] },
+    ScatterPlotData: { x: [], y: [], name: [], color: [] },
+    colors: {
+      tech: "#1f77b4",
+      health: "#ff7f0e",
+      bank: "#2ca02c",
+    },
   }),
 
   mounted() {
     this.fetchData();
   },
+
   methods: {
     async fetchData() {
       // req URL to retrieve companies from backend
       var reqUrl =
         "http://127.0.0.1:5000/companies?category=" +
         this.$props.selectedCategory;
-      console.log("ReqURL " + reqUrl);
-      // await response and data
       const response = await fetch(reqUrl);
       const responseData = await response.json();
 
@@ -44,8 +51,17 @@ export default {
         this.ScatterPlotData.name.push(company.name);
         this.ScatterPlotData.x.push(company.founding_year);
         this.ScatterPlotData.y.push(company.employees);
+
+        // Assign color based on the company's category
+        if (this.$props.selectedCategory === "All") {
+          this.ScatterPlotData.color.push(this.colors[company.category]);
+        } else {
+          this.ScatterPlotData.color.push(
+            this.colors[this.$props.selectedCategory]
+          );
+        }
       });
-      // after the data is loaded, draw the plot
+
       this.drawScatterPlot();
     },
     drawScatterPlot() {
@@ -56,7 +72,7 @@ export default {
         type: "scatter",
         text: this.ScatterPlotData.name,
         marker: {
-          color: "black",
+          color: this.ScatterPlotData.color,
           size: 12,
         },
       };
@@ -74,20 +90,12 @@ export default {
       var myPlot = document.getElementById("myScatterPlot");
       myPlot.on("plotly_click", function (data) {
         for (var i = 0; i < data.points.length; i++) {
-          // get the index of point
           let pn = data.points[i].pointNumber;
-
-          // emit event to change the currently selected company in the a) configuration panel
-          // and b) update the Profit View
           that.$emit("changeCurrentlySelectedCompany", pn + 1);
 
-          // revert all colors
           var colors = ["#00000" * 15];
-
-          // and change currently selected color to blue
           colors[pn] = "#3777ee";
 
-          // update the marker and plot
           var update = { marker: { color: colors, size: 12 } };
           Plotly.restyle("myScatterPlot", update);
         }
