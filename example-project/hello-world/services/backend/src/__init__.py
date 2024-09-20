d63547d3b2222eb86d5163d9ed4efe4db5aa18ae
Jakob Schwarz <jakob.schwarz97@gmail.com>
2024-09-20 08:55:09 +0200

    Setup backend

---
+++ example-project/hello-world/services/backend/src/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_restx import Resource, Api
+
+# instantiate the app
+app = Flask(__name__)
+api = Api(app)
+
+
+class Ping(Resource):
+    def get(self):
+        return {"status": "success", "message": "pong!"}
+
+
+api.add_resource(Ping, "/ping")
